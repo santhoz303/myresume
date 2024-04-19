4a4294ece1ed81fa9f9f11a1bf8c40d2a8213e21
dev.santhoz001@gmail.com <dev.santhoz001@gmail.com>
2024-04-20 01:40:37 +0530

    Added six components

--- MyResume/src/app/app.component.ts
+++ MyResume/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { SummaryComponent } from './summary/summary.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectComponent } from './project/project.component';
+import { EducationComponent } from './education/education.component';
+import { CourseComponent } from './course/course.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent, SummaryComponent, ExperienceComponent, ProjectComponent, EducationComponent, CourseComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
