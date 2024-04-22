24c487ee902b81ce0383ccde98957c303c8bf9d7
dev.santhoz001@gmail.com <dev.santhoz001@gmail.com>
2024-04-23 02:08:01 +0530

    Done model 02 done

--- MyResume/src/app/app.component.ts
+++ MyResume/src/app/app.component.ts
@@ -6,11 +6,14 @@ import { ExperienceComponent } from './experience/experience.component';
 import { ProjectComponent } from './project/project.component';
 import { EducationComponent } from './education/education.component';
 import { CourseComponent } from './course/course.component';
+import { AchievementsComponent } from './achievements/achievements.component';
+import { SkillsComponent } from './skills/skills.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent, SummaryComponent, ExperienceComponent, ProjectComponent, EducationComponent, CourseComponent],
+  imports: [RouterOutlet,HeaderComponent, SummaryComponent, ExperienceComponent, ProjectComponent, EducationComponent, CourseComponent, AchievementsComponent, SkillsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
